6aef764566f6fd1a4ca488d726c0e1879b407cf5
Giovani Garcia <abelgiovani@gmail.com>
2020-11-28 17:34:32 -0300

    Removido configuração do main para outro pacote

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,21 +1,20 @@
-package internal
+package config
 
 import (
 	"encoding/json"
-	"errors"
 	"os"
 )
 
 // configuration é um struct que representa o json de configuração
 type Configuration struct {
-	apiKey string
+	ApiKey string
 }
 
 func GetConfiguration() (*Configuration, error) {
 	var config Configuration
-	file, err := os.Open("configuration/config.development.json")
+	file, err := os.Open("config/config.development.json")
 	if err != nil {
-		return nil, errors.New("[Error]: não foi possível abrir o arquivo de configuração")
+		return nil, err
 	}
 	json.NewDecoder(file).Decode(&config)
 	return &config, nil

466f82d3c2e656cb5200d93486397a161a5338da
Giovani Garcia <abelgiovani@gmail.com>
2020-11-28 17:37:06 -0300

    implementada busca do giphy

--- go.mod
+++ go.mod
@@ -1,3 +1,19 @@
 module github.com/GamerSenior/delivery-much
 
 go 1.15
+
+require (
+	github.com/go-delve/delve v1.5.0 // indirect
+	github.com/google/go-dap v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/peterh/liner v1.2.0 // indirect
+	github.com/russross/blackfriday v1.6.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	github.com/spf13/cobra v1.1.1 // indirect
+	go.starlark.net v0.0.0-20201118183435-e55f603d8c79 // indirect
+	golang.org/x/arch v0.0.0-20201008161808-52c3e6f60cff // indirect
+	golang.org/x/sys v0.0.0-20201126233918-771906719818 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- pkg/api/giphy.go
+++ pkg/api/giphy.go
@@ -1,22 +1,59 @@
 package api
 
 import (
-	"fmt"
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+
+	"github.com/GamerSenior/delivery-much/internal/config"
 )
 
+type baseResponse struct {
+	Data []gif `json:"data"`
+}
+
+type gif struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	URL      string `json:"url"`
+	BitlyURL string `json:"bitly_url"`
+}
+
+// GetGifURLByTitle recebe uma string title e retorna a url
+// do primeiro GIF encontrado no GIPHY
 func GetGifURLByTitle(title string) (string, error) {
-	config, err := GetConfiguration()
+	config, err := config.GetConfiguration()
 	if err != nil {
 		return "", err
 	}
 
-	apiKey := config.apiKey
-	params := fmt.Sprintf("?api_key=%s&q=%s&limit=1", apiKey, title)
-	resp, err := http.Get("api.giphy.com/v1/gifs/search" + params)
+	apiKey := config.ApiKey
+	baseURL := "http://api.giphy.com/v1/gifs/search?"
+	v := url.Values{}
+	v.Set("api_key", apiKey)
+	v.Add("q", title)
+	v.Add("limit", "1")
+	baseURL += v.Encode()
+	resp, err := http.Get(baseURL)
 	if err != nil {
 		return "", err
 	}
+
 	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	var bResp baseResponse
+	if err := json.Unmarshal(data, &bResp); err != nil {
+		return "", err
+	}
+
+	if len(bResp.Data) > 0 {
+		return bResp.Data[0].URL, nil
+	}
 	return "", nil
 }
